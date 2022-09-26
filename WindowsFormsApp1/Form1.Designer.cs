9d4baa5d6a237cf99c9e349f81e5982282fb50f1
Kris <cherkezyan@itstep.academy>
2022-09-26 17:32:13 +0300

    добавили много кнопок

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -30,6 +30,9 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -42,11 +45,44 @@ namespace WindowsFormsApp1
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = false;
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Aqua;
+            this.button2.Location = new System.Drawing.Point(201, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 157);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Olive;
+            this.button3.Location = new System.Drawing.Point(30, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 157);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "OK";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button4.Location = new System.Drawing.Point(201, 201);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(156, 157);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "OK";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(862, 465);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +93,9 @@ namespace WindowsFormsApp1
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
